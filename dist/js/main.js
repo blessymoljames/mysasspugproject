9a79e2abd9418b7fb9f9d346c6265a6e9c456647
Blessymol <blessymolj@gmail.com>
2019-07-26 00:24:01 +0530

    first commit

---
+++ dist/js/main.js
@@ -0,0 +1,34 @@
+"use strict";
+
+console.log("haiiii"); //parallax js
+
+var $window = $(window);
+$('section[data-type="background"]').each(function () {
+  var $bgobj = $(this); // assigning the object
+
+  $(window).scroll(function () {
+    var yPos = -($window.scrollTop() / $bgobj.data('speed')); // Put together our final background position
+
+    var coords = '50% ' + yPos + 'px'; // Move the background
+
+    $bgobj.css({
+      backgroundPosition: coords
+    });
+  });
+}); //menu transition js
+
+$(window).scroll(function () {
+  var scroll = $(window).scrollTop();
+
+  if (scroll > 0) {
+    $(".navbar").addClass("navbar-scroll");
+  } else {
+    $(".navbar").removeClass("navbar-scroll");
+  }
+
+  if (scroll > 200) {
+    $(".navbar").addClass("bg-primary");
+  } else {
+    $(".navbar").removeClass("bg-primary");
+  }
+});
